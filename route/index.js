c0572a062c5ebcf0c4c2fcf09f0491931f85dce9
Dlyted1 <mookathy3@gmail.com>
2024-03-02 00:47:37 -0500

    created routes

---
+++ route/index.js
@@ -0,0 +1,8 @@
+Const routes = require('express').Router();
+const lesson1Controller = require('./controllers/lesson1');
+
+routes.get('/', lesson1Controller.JessicaRoute); 
+routes.get('/Chandler', lesson1Controller.ChandlerRoute); 
+routes.get('/Elyse', lesson1Controller.ElyseRoute); 
+
+modules.exports = routes;
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
+const lesson1Controller = require('../controllers/lesson1');
 
 const port = 3000;
 
+app.use('/', require('./routes/index'));
+
 /* app.use('/', require('./routes')); */
+/*
 app.get('/', lesson1Controller.JessicaRoute); 
-
 app.get('/Chandler', lesson1Controller.ChandlerRoute); 
-
 app.get('/Elyse', lesson1Controller.ElyseRoute); 
+moved to index.js */
 
 app.listen(process.env.port || port);
 console.log('Web Server is listening at port '+ (process.env.port || port));
