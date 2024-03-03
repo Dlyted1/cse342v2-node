e76e3f3cdfff6c04149a9aa8755afc27625f1cfb
Dlyted1 <mookathy3@gmail.com>
2024-03-03 02:53:26 -0500

    Project1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-const routes = require('express').Router();
+const router = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
 
-routes.get('/', lesson1Controller.JessicaRoute); 
-routes.get('/Chandler', lesson1Controller.ChandlerRoute); 
-routes.get('/Elyse', lesson1Controller.ElyseRoute); 
 
-module.exports = routes;
+router.get('/', lesson1Controller.JessicaRoute); 
+router.get('/Chandler', lesson1Controller.ChandlerRoute); 
+router.get('/Elyse', lesson1Controller.ElyseRoute); 
+
+router.use('/users', require('./users'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        
+        app.listen(port, () => {console.log(`Database is listening and node running on port ${port}`)});
+
+    }
+});
 
 /*
 app.get('/', lesson1Controller.JessicaRoute); 
@@ -11,5 +24,6 @@ app.get('/Chandler', lesson1Controller.ChandlerRoute);
 app.get('/Elyse', lesson1Controller.ElyseRoute); 
 moved to index.js */
 
+/*
 app.listen(process.env.port || port);
-console.log('Web Server is listening at port '+ (process.env.port || port));
+console.log('Web Server is listening at port '+ (process.env.port || port)); */
