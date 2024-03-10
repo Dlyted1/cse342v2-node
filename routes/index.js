523c73c224886dfe4dc6da3fc2d7947a8e30c965
Dlyted1 <mookathy3@gmail.com>
2024-03-10 05:34:25 -0400

    swagger added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
 
+router.use('/', require('./swagger'));
+router.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    //#swagger.tags=['users]
+    res.send('Hello World');
+})
 
 router.get('/', lesson1Controller.JessicaRoute); 
 router.get('/Chandler', lesson1Controller.ChandlerRoute); 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const mongodb = require('./data/database');
 
 const app = express();
@@ -7,9 +9,12 @@ const app = express();
 // const swaggerAutogen = require('swagger-autogen')();
 
 const port = process.env.PORT || 3000;
-
-app.use('/', require('./routes'));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow_Origin', '*');
+    next();
+})
+app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
     if(err) {
